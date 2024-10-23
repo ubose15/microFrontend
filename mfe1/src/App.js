bf590964051ca921c7c901d506ac3d323545d6d5
Urmi Bose <ubose15@gmail.com>
2024-10-23 22:26:25 +0530

    mfe1
    mfe1

---
+++ mfe1/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import App2 from 'app2/App'
+
+function App() {
+  return (<>
+    <div style={{
+      margin:'10px',
+      padding:'10px',
+      textAlign:'center',
+      backgroundColor:'cyan'
+    }}>
+
+      <h1>Application 1</h1>
+    </div><App2/>
+    </>
+    
+  );
+}
+
+export default App;
